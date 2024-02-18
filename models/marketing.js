5147188423a2de69a12dbcd10765dc36737109e0
Mack Ramirez <mnrdev510@gmail.com>
2024-02-18 09:16:34 -0800

    adding routes and data will test with Postman

--- models/marketing.js
+++ models/marketing.js
@@ -1,10 +1,31 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const kanbanSchema = new Schema({
+  boards: [
+    {
+      name: { type: String },
+      column: [
+        {
+          name: { type: String },
+          tasks: [
+            {
+              title: { type: String },
+              description: { type: String },
+              status: { type: String },
+              subtasks: [
+                {
+                  title: { type: String },
+                  isComplicated: Boolean,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
 
-})
-
-module.exports = mongoose.model("kanbanDBs", userSchema)
+module.exports = mongoose.model("kanbanDBs", kanbanSchema);
--- routes/marketingPlan.js
+++ routes/marketingPlan.js
@@ -1,7 +1,29 @@
 const marketRouter = require("express").Router();
+const Boards = require("../models/marketing");
 
 marketRouter.get("/", (req, res) => {
   res.send("THSI WORKS");
+});
+
+marketRouter.post("/marketing-plan", async (req, res) => {
+  try {
+    const marketPlan = new Boards({
+      name: req.params.name,
+      tasks: [
+        {
+          title: req.params.title,
+          description: req.params.description,
+          status: req.params.status,
+          subtasks: [
+            {
+              title: req.params.title,
+              isComplicated: req.params.isComplicated,
+            },
+          ],
+        },
+      ],
+    });
+  } catch (error) {}
 });
 
 module.exports = marketRouter;
